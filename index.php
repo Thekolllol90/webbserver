27ec9bcf0b82505956bb545635c2bcc2c81906b3
Thekolllol90 <neosvarre@hotmail.se>
2018-10-22 09:48:58 +0200

    del 5
    del 5

--- index.php
+++ index.php
@@ -4,6 +4,12 @@
         <meta charset="utf-8">
     </head>
     <body>
-    
+        <form action="submit.php" method="post">
+            Username: <input type="text" name="username">
+            Name: <input type="text" name="name">
+            Password: <input type="password" name="password">
+            Email: <input type="email" name="mail">
+        
+        </form>        
     </body>
 </html>
