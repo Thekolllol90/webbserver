3e1e7f51761ea46b2a6ad248d530e4991d65c672
Thekolllol90 <neosvarre@hotmail.se>
2018-10-25 09:25:35 +0200

    del 7
    del 7

--- index.php
+++ index.php
@@ -6,18 +6,26 @@
             form{
                 background-color: red;
                 padding: 20px;
-                width: 300px
+                width: 300px;
             }    
         </style>
     </head>
     <body>
-        <form action="submit.php" method="post"><br>
+        <form action="submit.php" method="post">
             Username:<br> <input type="text" name="username"><br>
             Name:<br> <input type="text" name="name"><br>
             Password:<br> <input type="password" name="password"><br>
             Email:<br> <input type="email" name="mail"><br><br>
-            <input type="submit" name="submit" value="registrera"><br><br>
-        </form>        
+            <input type="submit" name="submit" value="registrera"><br>
+        </form>   
+        
+             
+        <form action="submit.php" method="post">
+            Username:<br> <input type="text" name="username"><br>
+            Password:<br> <input type="password" name="password"><br><br>
+            <input type="submit" name="submit" value="logga in"><br>
+            
+        </form>     
     </body>
 </html>
 
